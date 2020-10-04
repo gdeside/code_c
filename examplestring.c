da5e87014bd0e7081da3eff051a77662e5b6e1da
gdeside <guillaume.deside@student.uclouvain.be>
2020-10-04 17:42:44 +0200

    string in c

---
+++ examplestring.c
@@ -0,0 +1,11 @@
+// C program to illustrate strings 
+  
+#include<stdio.h> 
+  
+int main() {    
+    // declare and initialize string 
+    char str[] = "Geeks"; 
+    // print string 
+    printf("%s\n",str); 
+    return 0; 
+} 
